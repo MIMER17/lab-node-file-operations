d18d5157a9617606b7dc9820cb0b80b4f0e1e432
mihir <mihirshetty9@gmail.com>
2021-09-13 01:20:27 +0530

    done

--- data.js
+++ data.js
@@ -1,1 +1,9 @@
-
+function data(name, year, qualification){
+    userData={
+        name:name,
+        Year:year,
+        Qualification:qualification
+    }
+    return userData
+}
+module.exports = data
